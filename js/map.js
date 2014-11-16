066bc7d138c79ac33e8064670894944975315ac1
unknown <hola@albertoblazquez.net>
2014-11-16 02:57:54 +0100

    Add Google Maps script

---
+++ js/map.js
@@ -0,0 +1,41 @@
+// The following example creates a marker in Stockholm, Sweden
+// using a DROP animation. Clicking on the marker will toggle
+// the animation between a BOUNCE animation and no animation.
+
+var stockholm = new google.maps.LatLng(59.32522, 18.07002);
+var parliament = new google.maps.LatLng(59.327383, 18.06747);
+var marker;
+var map;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 13,
+    center: stockholm,
+    disableDefaultUI: true
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  addMarker(); /* TODO */
+}
+
+function addMarker() {
+  marker = new google.maps.Marker({
+    map:map,
+    draggable:true,
+    animation: google.maps.Animation.DROP,
+    position: parliament
+  });
+
+  google.maps.event.addListener(marker, 'click', toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() != null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+/*google.maps.event.addDomListener(window, 'visible', addMarker);*/
